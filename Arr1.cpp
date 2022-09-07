ac4cba34cae3d1f2bfdcddcc5c40f34d59c8eb5c
codinghub1234 <112747789+codinghub1234@users.noreply.github.com>
2022-09-07 11:52:36 +0530

    Add files via upload

---
+++ Arr1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	int numbers[5] ;
+	cout<<"The numbers are: ";
+
+
+for (int i=0; i<5; ++i) {
+	cin>> numbers[i];
+}
+cout<<"The numbers are : ";
+
+for(int n=0;n<5;++n) {
+	cout<< numbers[n]<<" ";
+	}
+		return 0;
+}
